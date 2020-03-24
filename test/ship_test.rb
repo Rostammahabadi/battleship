22c265f484fe03528dce3fcc25e408152c86344d
Michael Gallup <Gallup93@icloud.com>
2020-03-23 19:33:15 -0600

    added test_hit method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -7,11 +7,16 @@ class ShipTest < Minitest::Test
   def setup
     @sub = Ship.new('sub', 2)
   end
-
+  #tests that instances of Ship initialze with a type, length, and health
   def test_initialization
     assert_equal 2, @sub.length
     assert_equal 'sub', @sub.ship_type
     assert_equal 2, @sub.health
+  end
+  #tests that hit method removes 1 from health
+  def test_hit
+    @sub.hit
+    assert_equal 1, @sub.health
   end
 
 end
