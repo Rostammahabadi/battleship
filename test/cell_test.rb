1001a934694e8dc6160ad4364acc783b93cbc728
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-24 08:00:51 -1000

    add coordinate return and no ship

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -11,4 +11,16 @@ class ShipTest < Minitest::Test
     assert_equal "B4", cell.coordinate
   end
 
+  def test_coordinate_return
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "B4", cell.coordinate
+  end
+
+  def test_there_is_no_ship
+    cruiser = Ship.new("Cruiser", 3)
+    
+    assert_equal nil, cell.ship
+  end
+
 end
