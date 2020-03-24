6d732a852c952262cbb5b6b94ab3dbaf2a4cd44a
Michael Gallup <Gallup93@icloud.com>
2020-03-23 23:19:12 -0600

    added cell.rb with initialize method

---
+++ lib/cell.rb
@@ -0,0 +1,11 @@
+class Cell
+  attr_reader :coordinate, :ship
+
+  #initialze with given coordinate name and other attributes
+  def initialize(cooridnate_name)
+    @coordinate = cooridnate_name
+    @ship = nil
+    @empty = true
+    @fired_upon = false
+  end
+end
