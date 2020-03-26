345cd788ca4f9ba441712e03ac0f00b4810ed8b7
Michael Gallup <Gallup93@icloud.com>
2020-03-26 08:21:36 -0600

    added board_test method for empty board and sink process of a ship

--- test/board_test.rb
+++ test/board_test.rb
@@ -74,4 +74,52 @@ class BoardTest < Minitest::Test
     refute board.valid_placement?(submarine, ["A1", "B1"])
   end
 
+  def test_board_render
+    #initialze instances
+    board = Board.new
+    submarine = Ship.new("Submaine", 2)
+    cruiser = Ship.new("Cruiser", 3)
+    #set board
+    board.place(cruiser, ["A1", "A2","A3"])
+    board.place(submarine, ["C4", "D4"])
+    #test render hides when false
+    no_whitespace = board.render
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "...", no_whitespace[5..7]
+    #test render shows ships when true
+    no_whitespace = board.render(true)
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "SSS", no_whitespace[5..7]
+    no_whitespace = board.render(true)
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "SS", no_whitespace[18] + no_whitespace[23]
+  end
+
+  def test_board_render_sink_process
+    #initialze instances
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["C2", "C3", "C4"])
+    #test board rendered sink process
+    board.cells["C1"].fire_upon
+    no_whitespace = board.render(true)
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "MSSS", no_whitespace[15..18]
+
+    board.cells["C2"].fire_upon
+    no_whitespace = board.render(true)
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "HSS", no_whitespace[16..18]
+
+    board.cells["C3"].fire_upon
+    no_whitespace = board.render(true)
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "HHS", no_whitespace[16..18]
+
+    board.cells["C4"].fire_upon
+    no_whitespace = board.render(true)
+    no_whitespace = no_whitespace.gsub(/\s+/, '')
+    assert_equal "XXX", no_whitespace[16..18]
+  end
+
 end
