274ac848ec8ea4b9c41126c33994fbff3e7be90b
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-26 09:58:25 -1000

    Adjusted the methods

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -5,85 +5,74 @@ require 'pry'
 
 class MainMenu
 
-  def initialize
+  def initialize()
+    computer_board = Board.new
+    computer_cruiser = Ship.new("Cruiser",3)
+    computer_submarine = Ship.new("Submarine", 2)
+    computer_board.place(computer_cruiser, ["A1","A2","A3"])
+    computer_board.place(computer_submarine, ["C1","C2"])
+    @user_board = Board.new
+  end
 
-
+  def start_game
     p "Welcome to BATTLESHIP"
     p "Enter p to play. Enter q to quit."
     enter = gets.chomp!
-#update to while
+    enter
+
     if enter == "p"
 
-      def create_computer_board_with_ships
-        computer_board = Board.new
-        computer_cruiser = Ship.new("Cruiser",3)
-        computer_submarine = Ship.new("Submarine", 2)
-        computer_board.place(computer_cruiser, ["A1","A2","A3"])
-        computer_board.place(computer_submarine, ["C1","C2"])
-      end
-
+    # def player_ship_placement
       puts "I have laid out my ships on the grid."
       puts "You now need to lay out your own two ships."
       puts "The Cruiser is three units long and the Submarine is two units long"
-      puts "#{Board.new.render(true)}"
+      puts "#{@user_board.render(true)}"
 
       p "Enter the squares for the Cruiser (3 spaces)"
       puts ">"
       cruiser = gets.chomp!
 
-      def create_user_board
-        user_board = Board.new
-      end
-
-      def create_player_cruiser_ship(cruiser)
-        cruiser_ship = Ship.new("Cruiser", 3)
+      cruiser_ship = Ship.new("Cruiser", 3)
+      cruiser_array = []
+      cruiser_array << cruiser[0..1]
+      cruiser_array << cruiser[3..4]
+      cruiser_array << cruiser[6..7]
+      while @user_board.valid_placement?(cruiser_ship, cruiser_array) == false
+        p "Those are invalid coordinates. Please try again: "
+        p ">"
+        cruiser = gets.chomp!
         cruiser_array = []
         cruiser_array << cruiser[0..1]
         cruiser_array << cruiser[3..4]
         cruiser_array << cruiser[6..7]
-        while user_board.valid_placement?(cruiser_ship, cruiser_array) == false
-          p "Those are invalid coordinates. Please try again: "
-          p ">"
-          cruiser = gets.chomp!
-          cruiser_array = []
-          cruiser_array << cruiser[0..1]
-          cruiser_array << cruiser[3..4]
-          cruiser_array << cruiser[6..7]
-        end
-        user_board.place(cruiser, cruiser_array)
       end
+      @user_board.place(cruiser_ship, cruiser_array)
+    end
+      p "Enter the squares for the Submarine (2 spaces)"
+      puts ">"
+      submarine = gets.chomp!
 
-      def create_player_submarine_ship_prompt
-        p "Enter the squares for the Submarine (2 spaces)"
+      submarine_ship = Ship.new("Submarine", 2)
+      submarine_array = []
+      submarine_array << submarine[0..1]
+      submarine_array << submarine[3..4]
+
+      while @user_board.valid_placement?(submarine_ship, submarine_array) == false
+        puts "Those are invalid coordinates. Please try again: "
         puts ">"
         submarine = gets.chomp!
-      end
-
-      def create_player_submarine_ship(submarine)
-        submarine_ship = Ship.new("Submarine", 2)
         submarine_array = []
         submarine_array << submarine[0..1]
         submarine_array << submarine[3..4]
-
-        while user_board.valid_placement?(submarine_ship, submarine_array) == false
-          puts "Those are invalid coordinates. Please try again: "
-          puts ">"
-          submarine = gets.chomp!
-          submarine_array = []
-          submarine_array << submarine[0..1]
-          submarine_array << submarine[3..4]
-        end
-          user_board.place(submarine_ship, submarine_array)
+      end
+        @user_board.place(submarine_ship, submarine_array)
+        binding.pry
       end
 
     end
 
-    if enter == "q"
-      return
-    end
+  #end
 
-  end
 
-end
-
-MainMenu.new
+first_game = MainMenu.new
+first_game.start_game
