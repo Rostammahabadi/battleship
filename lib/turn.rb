3ed6537d2a1394b9d08a5289d00817365ca46f1f
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-27 11:49:00 -1000

    deleted user and ai turns and created game turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,42 +4,22 @@ class Turn
 
   def initialize(human_board, ai_board)
     @turn_number = 0
-    @turn_type = "human"
     @human_board = human_board
     @ai_board = ai_board
     @ai_fire_order = @ai_board.cells.map {|cell| cell[0]}.shuffle
   end
 
-  def play_turn(turn_type)
-    if turn_type == "human"
-      human_turn
-      @turn_type = "ai"
-    else
-      ai_turn
-      @turn_type = "human"
-    end
-  end
 
-  def human_turn
-    puts "\n ------HUMAN TURN------"
-    puts @ai_board.render(true)
+  def game_turn
+    puts "=============COMPUTER BOARD============="
+    puts @ai_board.render(false)
+    puts "==============PLAYER BOARD=============="
+    puts @human_board.render(true)
     puts "Which coordinate would you like to strike?"
     strike_coordinates = gets.chomp
     @ai_board.cells[strike_coordinates].fire_upon
-    puts "\n ------RESULTS------"
-    puts @ai_board.render(true)
-    p "DAMAGE REPORT GOES HERE"
-  end
-
-  def ai_turn
-    puts "\n ------MR ROBOTO TURN------"
-    puts @human_board.render(true)
     @human_board.cells[@ai_fire_order[@turn_number]].fire_upon
     @turn_number += 1
-    puts "\n ------RESULTS------"
-    puts @human_board.render(true)
-
-    p "DAMAGE REPORT GOES HERE"
   end
 
   def final_turn?
