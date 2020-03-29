c86068e43bf3ce4d4d204c9428b2eb8be8662e9d
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-29 08:22:22 -1000

    refactor final_turn?

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,12 +32,7 @@ class Turn
     human_ship_cells = @human_board.cells_containing_ships.map {|key| @human_board.cells[key].fired_upon?}
     ai_ship_cells = @ai_board.cells_containing_ships.map {|key| @ai_board.cells[key].fired_upon?}
 
-    if (human_ship_cells.all? {|fired_upon_status| fired_upon_status == true}) == true ||
+    return true if (human_ship_cells.all? {|fired_upon_status| fired_upon_status == true}) == true ||
       (ai_ship_cells.all? {|fired_upon_status| fired_upon_status == true}) == true
-      puts "GAME OVER"
-      true
-    else
-      false
-    end
   end
 end
