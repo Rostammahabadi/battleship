69b9b6b44bc0890831ac1db9f4c0958d427f3522
Michael Gallup <Gallup93@icloud.com>
2020-03-31 21:45:50 -0600

    added test_no_repeated_coords method

--- test/verify_test.rb
+++ test/verify_test.rb
@@ -30,5 +30,16 @@ class VerifyTest < Minitest::Test
     assert_equal false, @verify.verify_no_overlap(["A1", "A2", "A3"], ["B1", "B2"])
   end
 
+  def test_no_repeated_coords
+    cell1 = Cell.new("A1")
+    cell2 = Cell.new("A2")
+    assert_equal true, @verify.verify_no_repeated_coordinate(cell1)
+    assert_equal true, @verify.verify_no_repeated_coordinate(cell2)
+    cell1.fire_upon
+    cell2.fire_upon
+    assert_equal false, @verify.verify_no_repeated_coordinate(cell1)
+    assert_equal false, @verify.verify_no_repeated_coordinate(cell2)
+  end
+
 
 end
