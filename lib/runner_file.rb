4096ab25c8a0a248b3df384eb569a466e39c14ef
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-31 17:11:53 -1000

    add while loop for game play

--- lib/runner_file.rb
+++ lib/runner_file.rb
@@ -1,4 +1,9 @@
 require './lib/main_menu'
+input = "y"
+while input == "y"
+  main = MainMenu.new
+  main.start_menu
 
-main = MainMenu.new
-main.start_menu
+  p "Would you like to play again? Press y to play again and n to quit."
+  input = gets.chomp
+end
